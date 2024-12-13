e862f439298e1ddd4861b409bc593bf36d3ed060
fati24la <lamfati2404@gmail.com>
2024-12-13 12:14:41 +0100

    affichage generatedCode

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import Modal from "./Modal";
+import CodeModal from "./CodeModal";
 import '../styles/navbar.css';
 import logoUml from '../logoUml.svg';
 import React, { useState } from 'react';
@@ -13,44 +14,106 @@ import { faPython } from '@fortawesome/free-brands-svg-icons';
 
 
 export default function Navbar({ showGenerateButton, onCreateDiagram }) {
-    const [showModal, setShowModal] = useState(false);
-    const openModal = () => setShowModal(true);
-    const closeModal = () => setShowModal(false);
+    const [modalType, setModalType] = useState(null); // Type de modal à afficher
+    const [codeContent, setCodeContent] = useState(""); // Contenu du code généré
+
+    const openModal = (type, code = "") => {
+        setModalType(type);
+        setCodeContent(code); // Définir le code uniquement si nécessaire
+    };
+
+    const closeModal = () => setModalType(null);
+
+    const codeExamples = {
+        java: `public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java!");
+      }
+     }`,
+        php: `<?php
+     echo "Hello, PHP!";
+    ?>`,
+        python: `print("Hello, Python!")`
+    };
+
+
     return (
         <>
             <nav className="navbar fixed-top navbar-expand-lg">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#">
-                        <img src={logoUml} alt="Logo" width="40" height="30" className="d-inline-block align-text-top" />
+                        <img src={logoUml} alt="Logo" width="40" height="30" className="d-inline-block align-text-top"/>
                         UmlDiagram
                     </a>
                     {showGenerateButton && (
-                        <div class="btn-group">
-                            <button class="btn btn-outline-success dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <div className="btn-group">
+                            <button className="btn btn-outline-success dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Generate code
                             </button>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#"><FontAwesomeIcon icon={faJava} /> Java</a></li>
-                                <li><a class="dropdown-item" href="#"><FontAwesomeIcon icon={faPhp} /> Php</a></li>
-                                <li><a class="dropdown-item" href="#"><FontAwesomeIcon icon={faPython} /> Python</a></li>
+                            <ul className="dropdown-menu">
+                                <li>
+                                    <a
+                                        className="dropdown-item"
+                                        href="#"
+                                        onClick={() => openModal('affichageCode', codeExamples.java)}
+                                    >
+                                        <FontAwesomeIcon icon={faJava} /> Java
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        className="dropdown-item"
+                                        href="#"
+                                        onClick={() => openModal('affichageCode', codeExamples.php)}
+                                    >
+                                        <FontAwesomeIcon icon={faPhp} /> PHP
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        className="dropdown-item"
+                                        href="#"
+                                        onClick={() => openModal('affichageCode', codeExamples.python)}
+                                    >
+                                        <FontAwesomeIcon icon={faPython} /> Python
+                                    </a>
+                                </li>
                             </ul>
                         </div>
                     )}
-                    <button className="btn btn-outline-primary" onClick={openModal}>
-                        New diagram <FontAwesomeIcon icon={faPlus} />
+                    <button className="btn btn-outline-primary" onClick={() => openModal('createDiagram')}>
+                        New diagram <FontAwesomeIcon icon={faPlus}/>
                     </button>
                 </div>
             </nav>
 
-            {/* La modal reste gérée ici */}
-            <Modal
-                isOpen={showModal}
-                closeModal={closeModal}
-                onCreateDiagram={() => {
-                    closeModal();
-                    onCreateDiagram(); // Notifie le parent
-                }}
-            />
+            {modalType === 'createDiagram' && (
+                <Modal
+                    isOpen={modalType === 'createDiagram'}
+                    closeModal={closeModal}
+                    title="New Diagram"
+                    type="createDiagram"
+                    onCreateDiagram={onCreateDiagram}
+                    className="create-diagram-modal"
+                >
+                    <form>
+                        <div className="mb-3">
+                            <label htmlFor="diagramName" className="form-label"><h5>Diagram name</h5></label>
+                            <input type="text" className="form-control" id="diagramName" />
+                        </div>
+                    </form>
+                </Modal>
+            )}
+
+            {/* CodeModal */}
+            {modalType === 'affichageCode' && (
+                <CodeModal
+                    isOpen={modalType === 'affichageCode'}
+                    closeModal={closeModal}
+                    codeContent={codeContent}
+                    className="code-modal"
+                />
+            )}
         </>
 
     )
